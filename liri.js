caaf0695c2390f66042db85abe997610f6795e6b
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-02-28 20:26:38 -0600

    updated files

--- liri.js
+++ liri.js
@@ -23,12 +23,12 @@ var Spotify = require('node-spotify-api');  //here we required the package and s
 var spotify = new Spotify(keys.spotify);  // "new" calls in a regular function so "function Spotify()" must be a CONSTRUCTOR FUNCTION
 console.log(spotify);
 
-spotify.search({
+//spotify.search({
 //There are two methods available, search and 'request'
 // "search is the EASIEST way to find an artist, album, or track"
 // example => search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
 
-})
+//})
 
 /*Console Log the following information about the song in your terminal/bash window
 Artist(s)
@@ -38,6 +38,38 @@ The album that the song is from */
 
 // ========================================================
 // ==== I need to send requests using the axios package ===
+//grab axios package & store it into a variable:
+var axios = require('axios');
+// Store all of the arguments in an array
+var spotifyArgs = process.argv;
+//empty variable will hold the name of the song requested by the user
+var userSongRequest = '';
+//loop through all the node arguments added by the user
+for(var i = 2; i <  spotifyArgs.length; i++) {
+    if(i > 2 && i < spotifyArgs.length) {
+        spotifyArgs = spotifyArgs + '+' + userSongRequest[i];
+    } 
+    else {
+        spotifyArgs += spotifyArgs[i];
+    }
+  }
+    //class example:
+    // Run a request with axios to the SPOTIFY API   
+    //var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    var queryUrl = 'https://api.spotify.com/v1/artists/' + spotify + userSongRequest;  //here I have to work with the parameters
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(
+      function(response) {
+        console.log('test: ' + response.data);
+      }
+    )
+    .catch(function (error) {
+      console.log(error);
+    });
+
+
+/*
 //example from documentation:
 axios.get('/user', {
     params: {
@@ -49,4 +81,4 @@ axios.get('/user', {
   })
   .catch(function (error) {
     console.log(error);
-  });
+  }); */
